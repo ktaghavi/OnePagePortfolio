713fc08ee0a458ec0968f340b062c3e4724a7364
Kamyar <caracicatriz.kt@gmail.com>
2024-05-08 18:05:56 -0700

    Initial commit

---
+++ portfolio-v2/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div id='main'>
+        <img className='' src='https://images.unsplash.com/photo-1663187180926-e0278b18139a?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>    </div>
+  )
+}
+
+export default Main
---
+++ portfolio-v2/src/components/Sidenav.jsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import {AiOutlineMenu, AiOutlineHome} from 'react-icons/ai';
+import { TbStack2 } from "react-icons/tb";
+import { FaLaptopCode } from "react-icons/fa";
+import { PiListMagnifyingGlass } from "react-icons/pi";
+import { BsSuitcaseLg } from "react-icons/bs";
+import { IoSchoolOutline } from "react-icons/io5";
+import { BsSend } from "react-icons/bs";
+
+
+const Sidenav = () => {
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
+  return (
+    <div>
+        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+        {
+            nav ? (
+                <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+                    <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineHome size={21} />
+                        <span className='pl-4'>Home</span>
+                    </a>
+                    <a href="#about" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <PiListMagnifyingGlass size={21} />
+                        <span className='pl-4'>About Me</span>
+                    </a>
+                    <a href="#stack" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <TbStack2 size={24} />
+                        <span className='pl-4'>Tech Stack</span>
+                    </a>
+                    <a href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <FaLaptopCode size={24} />
+                        <span className='pl-4'>Projects</span>
+                    </a>
+                    <a href="#experience" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <BsSuitcaseLg size={20} />
+                        <span className='pl-4'>Experience</span>
+                    </a>
+                    <a href="#education" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <IoSchoolOutline size={24} />
+                        <span className='pl-4'>Education</span>
+                    </a>
+                    <a href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <BsSend size={20} />
+                        <span className='pl-4'>Contact Me</span>
+                    </a>
+                </div>
+            )
+            : (
+            ''
+            )}
+            <div className='md:block hidden fixed top-[25%] z-10'>
+                <div className='flex flex-col'>
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineHome size={20}/>
+                    </a>
+                    <a href="#about" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <PiListMagnifyingGlass size={20}/>
+                    </a>
+                    <a href="#stack" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <TbStack2 size={20}/>
+                    </a>
+                    <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <FaLaptopCode size={20}/>
+                    </a>
+                    <a href="#experience" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <BsSuitcaseLg size={20}/>
+                    </a>
+                    <a href="#education" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <IoSchoolOutline size={20}/>
+                    </a>
+                    <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineHome size={20}/>
+                    </a>
+                </div>
+            </div>
+    </div>
+  );
+};
+
+export default Sidenav;
